489201f8f099ca1a2d396c1a0d20e8d507d267b2
Bairei <baireikawagishi@gmail.com>
2017-09-29 23:49:35 +0200

    MySQL branch, including dev/prod config and default H2 in-memory DB

--- src/main/kotlin/com/bairei/springrecipes/bootstrap/RecipeBootstrap.kt
+++ src/main/kotlin/com/bairei/springrecipes/bootstrap/RecipeBootstrap.kt
@@ -9,12 +9,14 @@ import com.bairei.springrecipes.repositories.RecipeRepository
 import com.bairei.springrecipes.repositories.UnitOfMeasureRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationListener
+import org.springframework.context.annotation.Profile
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import java.math.BigDecimal
 
 @Component
+@Profile("default")
 class RecipeBootstrap @Autowired constructor(val recipeRepository: RecipeRepository,
                                              val categoryRepository: CategoryRepository,
                                              val unitOfMeasureRepository: UnitOfMeasureRepository
