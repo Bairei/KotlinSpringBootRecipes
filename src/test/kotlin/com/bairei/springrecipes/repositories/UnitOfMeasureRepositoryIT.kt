665bf1aca95b316d0502e664dc91d9a0a68a640c
Bairei <baireikawagishi@gmail.com>
2017-09-30 09:50:19 +0200

    edit build.gradle file

--- src/test/kotlin/com/bairei/springrecipes/repositories/UnitOfMeasureRepositoryIT.kt
+++ src/test/kotlin/com/bairei/springrecipes/repositories/UnitOfMeasureRepositoryIT.kt
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner
 @RunWith(SpringRunner::class)
 @DataJpaTest
 class UnitOfMeasureRepositoryIT {
+
     @Autowired
     lateinit var unitOfMeasureRepository : UnitOfMeasureRepository
 
